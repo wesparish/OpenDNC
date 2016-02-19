86b3d923374190095a4f419d48bd113f47bbf17f
Wes Parish <wes@cowtownt.org>
2016-02-19 14:47:36 -0600

    Initial commit

---
+++ OpenDNC.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from lib import RS232DNC
+
+class OpenDNC:
+  def __init__(self):
+    pass
+
+if __name__ == "__main__":
+  print("OpenDNC Starting...")
+
+  print("OpenDNC Finished!")
