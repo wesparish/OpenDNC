23e5d9a4714ce83679c95c5f448f6a4dff648aad
Wes Parish <wes@cowtownt.org>
2016-05-02 17:03:30 -0500

    Setup for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'OpenDNC',
+  packages = ['OpenDNC'],
+  version = '0.4',
+  description = 'Open source CNC DNC (RS232) software',
+  author = 'Wes Parish',
+  author_email = 'wtopace@yahoo.com',
+  url = 'https://github.com/wesparish/OpenDNC',
+  download_url = 'https://github.com/wesparish/OpenDNC/archive/v0.4.tar.gz',
+  keywords = ['DNC', 'RS232', 'CNC', 'Machining'], # arbitrary keywords
+  classifiers = [],
+)
