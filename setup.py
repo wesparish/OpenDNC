63344bc2a795faaf16dd6879b0b4dcd676cdfc2a
Wes Parish <wes@cowtownt.org>
2016-05-02 17:05:05 -0500

    Bump tag version in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'OpenDNC',
   packages = ['OpenDNC'],
-  version = '0.4',
+  version = '0.5',
   description = 'Open source CNC DNC (RS232) software',
   author = 'Wes Parish',
   author_email = 'wtopace@yahoo.com',
   url = 'https://github.com/wesparish/OpenDNC',
-  download_url = 'https://github.com/wesparish/OpenDNC/archive/v0.4.tar.gz',
+  download_url = 'https://github.com/wesparish/OpenDNC/archive/v0.5.tar.gz',
   keywords = ['DNC', 'RS232', 'CNC', 'Machining'], # arbitrary keywords
   classifiers = [],
 )
